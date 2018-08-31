70e1440cc4900382c061e44a4d20912165f3ef8b
Yael Hernandez <42187113+yaelhdez@users.noreply.github.com>
2018-08-31 14:52:55 -0500

    Second Commit

--- main.c
+++ main.c
@@ -1,25 +1,30 @@
+//Carlos Yael Hernandez Alcala
+//A01412016
+//A01412016@ITESM.MX
+
 #include <stdio.h>
 
 int main(void) {
-    int hr;
+    int hr; //Declaration of the variable
 
     printf("Digame su hora en formato de 24h \n");
-    scanf("%d",&hr);
+    scanf("%d",&hr); //Variable hr request
 
-    if (hr>=0 & hr<24){
-        if (hr>=0 & hr<12){
+    if (hr>=0 & hr<24){ //this "if" evaluates whether the variable hr can be accepted
+        if (hr>=0 & hr<12){ //this "if" evaluates the variable "hr" to say "Buenos dias"
             printf("Buenos dias, son las %d horas",hr);
         }
-        else if (hr>=12 & hr<19){
+        else if (hr>=12 & hr<19){ //this "if" evaluates the variable "hr" to say "Buenas Tardes"
             printf("Buenas tardes, son las %d horas",hr);
         }
-        else if (hr>=19 & hr<24){
+        else if (hr>=19 & hr<24){ //this "if" evaluates the variable "hr" to say "Buenas Noches"
             printf("Buenas noches, son las %d horas",hr);
         }
 
     }
     else{
         printf("En este sistema humano no existen las %d horas",hr);
+        //If the condition of the main "if" is not true, then the program continues by this path
     }
     return 0;
 }
